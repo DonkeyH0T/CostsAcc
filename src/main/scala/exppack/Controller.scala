70e3b47d786855baa94d8cb7356881988a90776d
Dee <mario.huq@gmail.com>
2018-05-10 22:27:40 +0300

    исправил сигнатуру на более читаемую

--- src/main/scala/exppack/Controller.scala
+++ src/main/scala/exppack/Controller.scala
@@ -3,5 +3,5 @@ package exppack
 import scala.concurrent.Future
 
 trait Controller[Req, Res] extends (Req =>Future[Res])
-trait Controller2[ReqA1, ReqA2, Res] extends Function2[ReqA1, ReqA2, Future[Res]]
-trait Controller3[ReqA1, ReqA2, ReqA3, Res] extends Function3[ReqA1, ReqA2, ReqA3, Future[Res]]
+trait Controller2[ReqA1, ReqA2, Res] extends ((ReqA1, ReqA2) => Future[Res])
+trait Controller3[ReqA1, ReqA2, ReqA3, Res] extends ((ReqA1, ReqA2, ReqA3) => Future[Res])
