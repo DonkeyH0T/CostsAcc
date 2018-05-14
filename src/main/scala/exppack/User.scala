c85084ffe8e90871fd96d68000f5f7d806a388c0
mariohuq <mario.huq@gmail.com>
2018-05-15 00:39:49 +0300

    Переместил сущности предметной области (пользователь, трата) в отдельный `package`.

--- src/main/scala/exppack/User.scala
+++
@@ -1,3 +0,0 @@
-package exppack
-
-case class User(id: Int, name: String, pass: String)
---
+++ src/main/scala/exppack/domain/User.scala
@@ -0,0 +1,5 @@
+package exppack
+
+case class User(id: Int, name: String, pass: String)
+
+case class MaybeUser(name: String, pass: String)
