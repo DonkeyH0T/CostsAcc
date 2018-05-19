959d3e5073441dee661dc966b186a3ec33e958ee
DonkeyH0T <felloge@gmail.com>
2018-05-19 18:08:59 +0300

    доделал датарепозиторий

--- src/main/scala/exppack/MainObj.scala
+++ src/main/scala/exppack/MainObj.scala
@@ -3,12 +3,13 @@ package exppack
 import exppack.Controllers.{AddUserController, RemindController, UserController}
 import exppack.repository.MemoryUserRepository
 import exppack.domain.{Data, RegSample, User, UserRequest}
-import exppack.services.DbUserServiceImpl
+import exppack.services.{DbDataServiceImpl, DbUserServiceImpl}
 import org.joda.time._
 import slick.jdbc.{JdbcBackend, JdbcProfile, MySQLProfile}
 import slick.jdbc.JdbcBackend.Database
+
 import scala.concurrent.Future
-import scala.util.{Success, Failure}
+import scala.util.{Failure, Success}
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -18,8 +19,9 @@ object MainObj extends App{
   val profile = MySQLProfile
   val db = Database.forConfig("db")
   val dataService = new DbUserServiceImpl(profile,db)
-
+val serv = new DbDataServiceImpl(profile,db)
   //val f = dataService.getAll()
   val c = dataService.signIn(User("Misha","123"))
+  val p = serv.getAll()
   Await.ready(c, Duration.Inf)
 }
---
+++ src/main/scala/exppack/repository/DbDataRepository.scala
@@ -0,0 +1,32 @@
+package exppack.repository
+import exppack.domain.{Data, RegSample, Sample, User}
+import exppack.services.DbDataService
+import org.joda.time.DateTime
+
+import scala.concurrent.Future
+
+
+
+class DbDataRepository(dbService: DbDataService) extends DataRepository {
+
+  override def all(): Future[Seq[Data]] = dbService.getAll()
+
+  override def put(item: Data): Future[Data] = dbService.insert(item)
+
+  override def getRemind(user: User): Future[Seq[RegSample]] =   dbService.notify(user)
+
+  override def statByDate(dateFrom: DateTime, dateTo: DateTime, user: User): Future[Seq[Sample]] = {
+    dbService.filterByDate(dateFrom, dateTo, user)
+  }
+
+  override def sumByCategory(dateFrom: DateTime, dateTo: DateTime, category: String, user: User): Future[BigDecimal] = {
+    dbService.filterByCat(dateFrom, dateTo, category, user)
+  }
+
+  override def sumByShop(dateFrom: DateTime, dateTo: DateTime, shop: String, user: User): Future[BigDecimal] = {
+    dbService.filterByShop(dateFrom, dateTo, shop, user)
+  }
+}
+
+
+
