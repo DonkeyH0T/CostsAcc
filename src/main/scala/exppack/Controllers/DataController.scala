55781120224743f3e5fee895bd3f716b2db64cd7
mariohuq <mario.huq@gmail.com>
2018-05-20 19:12:52 +0300

    Изменил паттерн-матчинг на обычный вызов, так как сузил тип.

--- src/main/scala/exppack/Controllers/DataController.scala
+++ src/main/scala/exppack/Controllers/DataController.scala
@@ -1,21 +1,18 @@
 package exppack.Controllers
 
 import exppack.repository.DataRepository
-import exppack.domain.{Request, _}
+import exppack.domain.Request
 
 import exppack.domain.{RegSample, Sample}
 
 import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class AddDataController(implicit repository: DataRepository) extends Controller[Request, Boolean] {
 
-  override def apply(request: Request): Future[Boolean] = request match {
-    case Request.AddExpense(item, user) => repository.put(item.withUser(user)).map(_ => true)
-      .recover {
-        case _: Exception => false
-      }
-  }
+  override def apply(request: Request.AddExpense): Future[Boolean] = repository.put(request.exp.withUser(request.currentUser)).map(_ => true)
+    .recover {
+      case _: Exception => false
+    }
 }
 
 class GetStatController(implicit repository: DataRepository) extends Controller[Request, BigDecimal] {
@@ -47,7 +44,7 @@ class RemindController(implicit repository: DataRepository) extends Controller[R
   override def apply(request: Request): Future[Seq[RegSample]] = request match {
     case Request.WithDate(dateFrom, dateTo, user) => user match {
       case Some(u) =>
-        repository.getReminds(u)
+        repository.getRemind(u)
     }
   }
   
